fa163c43a88ff76a1099f59583e323989ac6e4ac
Ivan Bega <bega.ivan03@gmail.com>
2022-10-31 19:02:40 +0300

    tests for queue

--- tasks/task6/src/main/java/me/bega/Main.java
+++ tasks/task6/src/main/java/me/bega/Main.java
@@ -1,9 +1,29 @@
 package me.bega;
 
 public class Main {
-    public static void main(String[] args) {
-        MichaelScottQueue<Integer> queue = new MichaelScottQueue<Integer>();
-        queue.enqueue(5);
-        System.out.println(queue.dequeue());
+    static MichaelScottQueue<Integer> queue = new MichaelScottQueue<Integer>();
+    public static void main(String[] args) throws InterruptedException {
+        test();
+        for (int i = 0; i < 2000; i++) {
+            System.out.println(queue.dequeue());
+        }
+    }
+
+    private static void test() throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                queue.enqueue(i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 1000; i < 2000; i++) {
+                queue.enqueue(i);
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
     }
 }
--- tasks/task6/src/test/java/me/bega/MichaelScottQueueTest.java
+++ tasks/task6/src/test/java/me/bega/MichaelScottQueueTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MichaelScottQueueTest {
     private MichaelScottQueue<Integer> queue;
 
@@ -17,5 +20,60 @@ public class MichaelScottQueueTest {
         queue.enqueue(1);
         int actual = queue.dequeue();
         Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    void testEnqueueElementsConcurrently() throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                queue.enqueue(i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 1000; i < 2000; i++) {
+                queue.enqueue(i);
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < 2000; i++) {
+            result.add(queue.dequeue());
+        }
+
+        for (int i = 0; i < 2000; i++) {
+            Assertions.assertTrue(result.contains(i));
+        }
+    }
+
+    @Test
+    void testDequeueElementsConcurrently() throws InterruptedException {
+        // add 21 - remove 20
+        for (int i = 0; i < 21; i++) {
+            queue.enqueue(i);
+        }
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                queue.dequeue();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                queue.dequeue();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
+        int last = queue.dequeue();
+        Assertions.assertNull(queue.dequeue());
+        Assertions.assertTrue(last >=0 && last < 21);
     }
 }
