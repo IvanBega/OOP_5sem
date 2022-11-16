98a672792d122c2daa73515d03ec7b70092a8991
Ivan Bega <bega.ivan03@gmail.com>
2022-11-17 01:07:20 +0200

    CyclicBarrier tests

--- tasks/task7/src/test/java/me/bega/cyclicbarrier/CyclicBarrierTest.java
+++ tasks/task7/src/test/java/me/bega/cyclicbarrier/CyclicBarrierTest.java
@@ -1,4 +1,57 @@
 package me.bega.cyclicbarrier;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.BrokenBarrierException;
+
 public class CyclicBarrierTest {
+    private CyclicBarrierInterface cyclicBarrier;
+    private boolean finished = false;
+    @BeforeEach
+    public void setup() {
+        cyclicBarrier = new CyclicBarrier(3, () -> finished = true);
+    }
+    private void makeThread() {
+        Thread thread = new Thread(() ->{
+            try {
+                cyclicBarrier.await();
+            } catch (BrokenBarrierException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+    @Test
+    public void testAllThreadsReachBarrier() throws InterruptedException {
+        makeThread();
+        makeThread();
+        makeThread();
+        synchronized (this) {
+            this.wait(1000);
+        }
+        Assertions.assertTrue(finished);
+    }
+    @Test
+    public void testOneThreadRemainingToReachBarrier() throws InterruptedException {
+        makeThread();
+        makeThread();
+        synchronized (this) {
+            this.wait(2000);
+        }
+        Assertions.assertEquals(1, cyclicBarrier.getNumberWaiting());
+    }
+    @Test
+    public void testBarrierRelease() throws InterruptedException {
+        makeThread();
+        makeThread();
+        makeThread();
+        synchronized (this) {
+            this.wait(1000);
+        }
+        Assertions.assertTrue(finished);
+        Assertions.assertEquals(3, cyclicBarrier.getNumberWaiting());
+    }
+
 }
