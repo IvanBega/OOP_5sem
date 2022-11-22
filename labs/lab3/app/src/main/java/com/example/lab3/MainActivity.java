63dfb2932c894858b99a0db256205e34a9474af3
Ivan Bega <bega.ivan03@gmail.com>
2022-11-22 19:29:20 +0200

    basic layout

--- labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ labs/lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
     }
 }
